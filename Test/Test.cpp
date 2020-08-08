4dd85dc23da82a952f2fa46c6d2102fa7ded6667
Johlver Pardo <jjpardo2002@gmail.com>
2020-08-08 15:40:10 -0500

    Commit Native

---
+++ Test/Test.cpp
@@ -0,0 +1,5 @@
+﻿// Test.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "pch.h"
+#include "Test.h"
---
+++ Test/dllmain.cpp
@@ -0,0 +1,28 @@
+﻿// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+#include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
+#define DllExport __declspec (dllexport)
+extern "C"
+{
+	
+	DllExport const char* Reversar(char* nombre)
+	{
+			char ch, *p, *q;
+
+			for (q = nombre; *q != '\0'; ++q)
+				;
+			if (q > nombre)
+				--q;
+  
+
+		  for (p = nombre; p < q; ++p, --q) {
+			ch = *p;
+			*p = *q;
+			*q = ch;
+		  }
+		return nombre;
+	}
+
+}
